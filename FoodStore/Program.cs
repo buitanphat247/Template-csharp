2d4a83d09cf83709f98f567e5e8f0138c6b55fea
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2025-10-08 09:34:18 +0700

    initial structure

---
+++ FoodStore/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // config utf-8 for language vietnam
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Xin chào mọi người!");
+    }
+}
